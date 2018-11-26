d02ff91bb68bd402b1a2d9e104b0556081d7b8f5
Yauheni Tsiarletski <yauheni_tsiarletski@epam.com>
2018-11-26 10:19:55 +0300

    fixed task1.js

--- task1/Task1.js
+++ task1/Task1.js
@@ -1,14 +1,14 @@
-
+/* eslint-disable */
 const yargs = require("yargs");
 const fs = require("fs");
-
+/* eslint-enable */
 const titleOptions = {
-    describe: "Title of note",
-    demandOption: true,
+  describe: "Title of note",
+  demandOption: true,
 };
 const bodyOptions = {
-    describe: 'Body of note',
-    demandOption: true,
+  describe: 'Body of note',
+  demandOption: true,
 };
 
 yargs.command("add", "Add a new note",
@@ -67,122 +67,78 @@ yargs.command("remove", "Remove a note",
   .argv;
 
 function addNote(argv) {
-        
-  //   if (fs.existsSync("Notes.json")) {
-  //     let note = {title: argv.title, body: argv.body};
-  // //     notes = JSON.stringify(notes, null,"\t"); 
-  //     let json_file = fs.readFileSync("Notes.json", "utf8");
-  //     console.log(json_file)
-      // json_file.split("\r\n");
-      // json_file = JSON.parse(json_file);
-      // console.log(json_file);
-      // json_file.push(note);
-      // json_file = JSON.stringify(note,null,"\t")
-      // fs.writeFileSync("Notes.json", json_file, "utf8");
-      // json_file = JSON.parse(json_file);
-      
-
-      // json_file.push();
-      // console.log(json_file);
-      
-  //     fs.writeFileSync("Notes.json", array_file, "utf8");
-      
-      // let note = {title: argv.title, body: argv.body};
-      // json_file = JSON.stringify(note, null,"\t");
-      // fs.readFileSync("Notes.json", "utf8");
-      // fs.appendFileSync("Notes.json", "\n" + json_file);
-      // console.log("Note added");
-    // } else {
-    let mass = [];
-    let notes = {title: argv.title, body: argv.body};
-    mass.push(notes);
-    json_file = JSON.stringify(mass, null,"\t");
-    //console.log(json_file);
-    fs.writeFileSync("Notes.json", json_file, "utf8"); 
-    console.log("Notes.json file was created");
+  if (fs.existsSync("Notes.json")) {
+    const note = { title: argv.title, body: argv.body };
+    const json_file = fs.readFileSync("Notes.json", "utf8");
+    let obj = JSON.parse(json_file);
+    obj.push(note);
+    obj = JSON.stringify(obj, null, "\t");
+    fs.writeFileSync("Notes.json", obj, "utf8", () => {
+    });
+    // eslint-disable-next-line
+    console.log("The note added");
+  } else {
+    const notes = [{ title: argv.title, body: argv.body }];
+    const jsonStr = JSON.stringify(notes, null, "\t");
+    fs.writeFileSync("Notes.json", jsonStr, "utf8", () => {
+    });
+    // eslint-disable-next-line
+    console.log("Notes.json was created");
   }
-
-
-
-        
-    
-   
-    
-    
-  
-//   let json_file = fs.readFileSync("Notes.json", "utf8");
-//   let json_string = json_file.split("\r\n"); 
-  
-    
-  
-  
-//     let json_string = JSON.parse(json_file);
-//     console.log(json_file);
-//     console.log(json_string);
-//     json_string.push({ title: argv.title, body: argv.body });
-//     console.log(json_string);
-//     fs.writeFile("Notes.json", JSON.stringify(json_string, null, "\t"), "utf8", () => {
-
-//       console.log("Notes.json file was written");
-  
- 
-
-
-
-
+}
 
 function listAllNotes() {
+  // eslint-disable-next-line
   const json_file = require("./Notes.json");
   json_file.forEach(function (arr) {
+    // eslint-disable-next-line
     console.log(arr);
   });
 }
 
 function readNote(argv) {
+  // eslint-disable-next-line
   const json_file = require("./Notes.json");
   const result = json_file.filter(function (arr) {
     return arr.title === argv.title;
   });
+  // eslint-disable-next-line
   console.log(result);
 }
 
 function removeNote(argv) {
+  // eslint-disable-next-line
   const json_file = require("./Notes.json");
   const result = json_file.forEach(function (arr) {
     if (arr.title === argv.title) delete json_file[arr];
   });
-  fs.writeFile("Notes.json", JSON.stringify(result), "utf8", () => {
-
+    fs.writeFile("Notes.json", JSON.stringify(result), "utf8", () => {
+    // eslint-disable-next-line
     console.log("The note was removed");
   });
 }
 
 function hasFileExist() {
-  fs.existsSync("Notes.json") ? console.log ("Notes.json file exists" ) : console.log("Notes.json file not found");
+  if (fs.existsSync("Notes.json")) {
+    // eslint-disable-next-line
+    console.log("Notes.json file exists");
+  } else {
+    throw new Error("Notes.json file not found");
+  }
 }
-
-
-
-
-    
-
-        
-    
-
-    
   
-//   } else if (command === 'list') {
-//     // do something to list all notes
-//   } else if (command === 'read') {
-//     // do something to read a specific note
-//     // argv.title = requested note title
-//   } else if (command === 'remove') {
-//     // do something to remove a specific note
-//     // argv.title = requested note title
-//   } else {
-//       console.log("invalid command")
-//   }
+//   function checkToEqualTitle() {
+//     const json_file = require("./Notes.json");
 
-  
-    
+
+//   const result = json_file.forEach(function (arr) {
+//     if (arr.title !== 'undefined') { 
+//       let popped = json_file.pop();
+//       delete popped;
+//     }
+//       fs.writeFile("Notes.json", JSON.stringify(result), "utf8", () => {
+//         throw new Error("This title exists, сhange one.");
+//     });
+//   })
+// }
 
