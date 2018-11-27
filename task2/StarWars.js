4e36a51a6d9a1562387606042377b4666d0112a4
terletski <39872695+terletski@users.noreply.github.com>
2018-11-27 20:19:03 +0300

    update task2

--- task2/StarWars.js
+++ task2/StarWars.js
@@ -1,57 +1,82 @@
+const fetch = require('node-fetch');
+const yargs = require('yargs');
+const fs = require('fs');
 
-const fetch = require('fetch').fetchUrl;
-console.log('Write number from 1 to 87 to choose a character');
-let ask = 2;
-
-// console.log("ok");
-fetch('http://swapi.co/api/people/' + ask + '/')
-  .then(res => res.json())
-  .then(function (res) {
-    // console.log(res); all scope from swapi
-    let name = res.name;
-
-    // console.log(name);
-    let species = res.species;
-    let films = res.films;
-    // console.log(species);
-
-    let getSpecies = species.map(specie => fetch(specie).then(result => result.json()));
-    // console.log(getSpecias);
-    return Promise.all(getSpecies).then(res => ({ name, films, species: res }));
+yargs
+  .command('choose <number>', 'Choose character', (yargs) => {
+    yargs.positional('number', {
+      type: 'number',
+      default: '1',
+      description: 'To choose a character number must be from 1 to 87'
+    })
+  }, function (argv) {
+    chooseCharacter(argv.number);
   })
+  .help()
+  .argv;
 
-  .then(function (res) {
-    let name = res.name;
-    let species = res.species;
-    let films = res.films;
-    let getFilms = films.map(film => fetch(film).then(result => result.json()));
-    return Promise.all(getFilms).then(res => ({ name, species, films: res }));
-  })
+function chooseCharacter(number) {
+  // console.log("ok");
+  fetch('http://swapi.co/api/people/' + number + '/')
+    .then(response => response.json())
+    .then(function (response) {
+      // console.log(response); all scope from swapi
+      let name = response.name;
+      // console.log(name);
+      let species = response.species;
+      let films = response.films;
+      // console.log(species);
+      let getSpecies = species.map(specie => fetch(specie).then(result => result.json()));
+      // console.log(getSpecias);
+      return Promise.all(getSpecies).then(response => ({ name, films, species: response }));
+    })
 
-  .then(function (res) {
-    let name = res.name;
-    let species = res.species;
-    let films = res.films;
-    let people = res.species[0].people;
-    // console.log(people);
-    let getPeople = people.map(person => fetch(person).then(result => result.json()));
-    return Promise.all(getPeople).then(res => ({ name, species, films, people: res }));
-  })
+    .then(function (response) {
+      let name = response.name;
+      let species = response.species;
+      let films = response.films;
+      let getFilms = films.map(film => fetch(film).then(result => result.json()));
+      return Promise.all(getFilms).then(response => ({ name, species, films: response }));
+    })
 
-  .then(function (res) {
-    let name = res.name;
-    let species = res.species;
-    let films = res.films;
-    let people = res.people;
+    .then(function (response) {
+      let name = response.name;
+      let species = response.species;
+      let films = response.films;
+      let people = response.species[0].people;
+      // console.log(people);
+      let getPeople = people.map(person => fetch(person).then(result => result.json()));
+      return Promise.all(getPeople).then(response => ({ name, species, films, people: response }));
+    })
 
-    console.log(`
-                ${name}
-                ${films.map(film => 'film: ' + film.title).join('\n')}
-                ${species.map(specie => 'species: ' + specie.name).join('\n')}
-                ${people.map(person => 'people: ' + person.name).join('\n')}
-                `);
-  })
+    .then(function (response) {
+      let name = response.name;
+      let species = response.species;
+      let films = response.films;
+      let people = response.people;
 
-  .catch(function (err) {
-    throw new Error(err);
-  });
+      let print = `${name}
+                   ${films.map(film => 'film: ' + film.title).join('\n')}
+                   ${species.map(specie => 'species: ' + specie.name).join('\n')}
+                   ${people.map(person => 'people: ' + person.name).join('\n')}`;
+                  return print;
+    })
+
+    .then(function (print) {
+      // write to file
+      let buf = Buffer.from(print);
+      buf.write('');
+      fs.writeFile('./star_wars.json', print, 'utf8', (err) => {
+        if (err) throw err;
+      });
+        console.log("country.json was created");
+    })
+
+    .catch(function (err) {
+      throw new Error(err);
+    });
+}
+
+
+
+
